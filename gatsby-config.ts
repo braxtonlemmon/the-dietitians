041196a61eec3203c7935f5808d1949005cc7acf
braxtonlemmon <braxtonlemmon@gmail.com>
2024-01-29 13:22:16 -0700

    update seo keywords

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,8 +12,7 @@ const config: GatsbyConfig = {
       'Nutrition counseling, medical nutrition therapy, diabetes education',
     siteUrl: `https://www.komplete-nutrition.com`,
     image: '/favicon.png',
-    keywords:
-      'nutrition, diabetes, body image, disordered eating, dietitian, registered dietitian, weight loss, outpatient, meal plan, food allergies, intuitive eating',
+    keywords: `nutrition, diabetes, body image, disordered eating, dietitian, registered dietitian, weight loss, outpatient, meal plan, food allergies, intuitive eating, ozempic, wyngovy, semaglutide, glp-1, mounjaro, rysbelsus, phentermine, weight loss, weight loss drugs, keto, diet, intermittent fasting, bariatric surger evaluations, celiac, chronic kidney disease, constipation, diarrhea, stomach pain, crohn's disease, disordered eating, ehlers danlos syndrome, fatty liver disease, cirrhosis, gastroparesis, IBS, POTS, SIBO, sucrase isomaltase enzyme deficiencies, superior mesenteric artery syndrome, ulcerative colitis, radically open dialectical behavior therapy, acceptance commitment therapy, body image, vertical sleeve gastrectomy, roux-en-Y gastric bypass, billopancreatic diversion, billopancreatic diversion with duodenal switch, adjustable gastric band, gastric sleeve, gastric bypass, trauma, medical nutrition therapy, registered dietitian, cognitive behavior therapy, dialectical behavior therapy, borderline personality disorder, bipolar disorder, enteral feeding, tube feeding, malnutrition, PICA, night eating disorder, rumination, type 2 diabetes, type 1 diabetes, outpatient, bad body image, blood sugar, insulin, continuous glucose monitor, gluten free, hosimotos disease, dairy free, vegetarian, vegan, food allergies, intuitive eating, health at every size, meal plan `,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
